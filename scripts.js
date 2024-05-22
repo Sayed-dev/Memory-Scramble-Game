d56d26c168381e660c6e8453ebd41c2fbda6013b
Sayed Badran <s.mahmoud-c@tamkeentech.sa>
2024-05-22 12:51:15 +0300

    add checkForMatch and unflipCards JS functions

--- scripts.js
+++ scripts.js
@@ -11,11 +11,30 @@ document.addEventListener('DOMContentLoaded', () => {
 // flipCard by Bisho
 
 // checkForMatch by Sayed
-
+function checkForMatch() {
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+    isMatch ? disableCards() : unflipCards();
+    updateTries();
+    if (isMatch) {
+        matches++;
+        if (matches === totalPairs) {
+            alert('Congratulations! You completed the game!');
+        }
+    }
+}
 // disableCardsby Bisho
 
 // unflipCards by sayed
+function unflipCards() {
+    lockBoard = true;
 
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
 //updateTries by sayed
 
 // resetBoard by sayed
